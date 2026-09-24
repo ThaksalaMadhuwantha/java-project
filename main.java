fc0bc2312b6d15136d02c5e8811b34a4eb6de7a8
Thaksala Madhuwantha <thaksalauni@gmail.com>
2026-09-25 02:16:13 +0530

    Good Night

--- main.java
+++ main.java
@@ -2,10 +2,7 @@ public class main{
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("I am a hacker");
-<<<<<<< HEAD
         System.out.println("tdtdgdghfgud");
-=======
-        System.out.println("gopalu paka");
->>>>>>> 30f5e6cccfc87c1bfa39df8bcf4a2da1b878bb07
+        System.out.println("දැන් නිදාගමු....Good Night");
     }
 }
