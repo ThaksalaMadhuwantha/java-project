9fd79aa8a313d5ddcfc4d2c48919d9c45771b214
Thaksala Madhuwantha <thaksalauni@gmail.com>
2026-09-25 12:14:50 +0530

    Equations...

---
+++ Activity_Set_01/Euations.java
@@ -0,0 +1,9 @@
+package Activity_Set_01;
+
+public class Euations {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println("Sum is = "+(num1+num2));
+    }
+}
