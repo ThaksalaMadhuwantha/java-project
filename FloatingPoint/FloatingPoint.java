d0628ef671037d425b0aabbc3945625ef0588fb5
Thaksala Madhuwantha <thaksalauni@gmail.com>
2026-09-25 12:04:17 +0530

    Printing Floating Numbers😁

--- FloatingPoint/FloatingPoint.java
+++ FloatingPoint/FloatingPoint.java
@@ -3,5 +3,7 @@ public class FloatingPoint {
     public static void main(String[] args) {
         double number1 = 3.41;
         float number2 = 3.41F;
+        System.out.println("Noramal number- "+number1);
+        System.out.println("Floting Number- "+number2);
     }
 }
