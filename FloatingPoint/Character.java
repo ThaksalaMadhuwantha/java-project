468e0d2ad55cd1650a9dd5497f8792d2a4fedb11
Thaksala Madhuwantha <thaksalauni@gmail.com>
2026-09-25 12:06:13 +0530

    Character

---
+++ FloatingPoint/Character.java
@@ -0,0 +1,8 @@
+package FloatingPoint;
+
+public class Character {
+    public static void main(String[] args) {
+        char letter = 'A';
+        System.out.println("Character- "+letter);
+    }
+}
