ccce11a44f0243e43fe6785dc1eb531c0b1423a6
Thaksala Madhuwantha <thaksalauni@gmail.com>
2026-09-25 14:41:01 +0530

    rectangle area

---
+++ Activity_Set_01/findArea.java
@@ -0,0 +1,11 @@
+package Activity_Set_01;
+
+public class findArea {
+    public static void main(String[] args) {
+        double length = 10;
+        double width = 5;
+        double area = length*width;
+
+        System.out.println("Area of rectangle : "+ area);
+    }
+}
