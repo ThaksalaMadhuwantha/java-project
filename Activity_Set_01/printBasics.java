90afaf5f0515f84a71e4f24f986ee32909bf650f
Thaksala Madhuwantha <thaksalauni@gmail.com>
2026-09-25 14:03:58 +0530

    print basic with variables

---
+++ Activity_Set_01/printBasics.java
@@ -0,0 +1,13 @@
+package Activity_Set_01;
+
+public class printBasics {
+    public static void main(String[] args) {
+        String name ="Thaksala Madhuwantha";
+        int age = 23;
+        double GPA =5.5;
+
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+        System.out.println("GPA : "+GPA);
+    }
+}
