9f937ba1a0dcfc9825dcdc3c65e649263ef7c646
Thaksala Madhuwantha <thaksalauni@gmail.com>
2026-09-25 11:52:49 +0530

    Start Codings...

--- main.java
+++ main.java
@@ -1,8 +1,5 @@
 public class main{
     public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.println("I am a hacker");
-        System.out.println("tdtdgdghfgud");
-        System.out.println("දැන් නිදාගමු....Good Night");
+        System.out.println("Hellow world");
     }
 }
